cbba964b8c7b4d5f86692341fcf42dec1312091b
Paulo <pauloamino76@gmail.com>
2025-01-22 19:36:04 -0300

    feat(py): Adding default regions

--- source/defaults/regions.py
+++ source/defaults/regions.py
@@ -5,14 +5,21 @@ from utils import debug
 def regions(db: Database):
     try:
         # Adding regions
-        default_regions = [
-            ("Vilarejo do Amanhecer", "Região inicial do jogar, um vilarejo tranquilo, esbelto...", "Água")
+        regions = [
+        ("Vilarejo do Amanhecer", "Uma vila tranquila, onde os primeiros raios de sol tocam a terra.", "Água"),
+        ("Floresta Eterna", "Uma floresta densa e mística, habitada por espíritos antigos.", "Terra"),
+        ("Ruínas do Abismo", "Ruínas ancestrais com segredos esquecidos.", "Trevas"),
+        ("Deserto de Areias Infinitas", "Um deserto vasto e sem fim, onde segredos estão enterrados sob a areia.", "Fogo"),
+        ("Montanha do Crepúsculo", "Uma montanha alta e gélida, coberta de mistérios e perigos.", "Ar"),
+        ("Terras Devastadas", "Um lugar sombrio e inóspito, marcado pela destruição e caos.", "Trevas"),
+        ("Caverna Cristalizada", "Uma caverna mágica repleta de cristais brilhantes e perigos ocultos.", "Luz"),
+        ("Caverna Soterrada", "Uma caverna misteriosa enterrada sob as montanhas, cheia de segredos antigos.", "Terra")
         ]
         db.cur.executemany(
             """
             INSERT INTO regiao (nome, descricao, elemento)
             VALUES (%s, %s, %s)
-            """, default_regions
+            """, regions
         )
 
         debug("default: Regions added successfully!")
