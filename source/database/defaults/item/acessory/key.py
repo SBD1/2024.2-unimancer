b7a97241ffc19cd1e8417e62f723dd0ddda82d4f
João F. (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2025-01-27 03:25:33 -0300

    feat(items): add keys of game
    Co-authored-by: NATAN ALMEIDA <natanalmeida03@users.noreply.github.com>
    Co-authored-by: Renan Vieira  <R-enanVieira@users.noreply.github.com>
    Co-authored-by: Millena Queiroz <MillenaQueiroz@users.noreply.github.com>

---
+++ source/database/defaults/item/acessory/key.py
@@ -0,0 +1,52 @@
+from database import Database
+from utils import debug, error
+from colorama import Style
+
+# Add hats in the magical fantasy game where only exists magical creatures.
+def keys(db: Database):
+    
+    table_name = Style.BRIGHT + "ACESSORIO (Chave)" + Style.NORMAL
+    
+    try:
+        # [("descricao...", inimigos para matar para dropar, "nome", "peso", "preco")]
+        default_values = [
+            # Abre a "Catedral Queimada".
+            (
+                "Cruz Papal",
+                9999999,
+                "Antiga cruz deixada por um portão da igreja.",
+                1,
+                100
+            ),
+            # Abre a "Fenda do Abismo"
+            (
+                "Chave de Bronze",
+                9999999,
+                "uma chave de bronze que abre a porta para o Abismo.",
+                2,
+                78
+            ),
+            # Abre o "Monte Caído".
+            (
+                "Torcha do Escuro Montante",
+                9999999,
+                "uma tocha que brilha no escuro especificamente no Monte Caído.",
+                5,
+                128
+            )
+        ]
+        
+        db.cur.executemany(
+            """
+            SELECT criar_acessorio('Chave', %s, %s, %s, %s, %s);
+            """, default_values
+        )
+
+        db.conn.commit()
+        debug(f"default: {len(default_values)} {table_name} added successfully!")
+        
+        return len(default_values)
+
+    except Exception as e:
+        db.conn.rollback()
+        error(f"default: error occurred while adding {table_name}s values: {e}")
